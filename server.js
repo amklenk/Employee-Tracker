1cea560e754f71c398fe561828e53111227e426a
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-29 13:27:05 -0400

    added add and delete queries for roles table in server

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ console.log(rows);
 //     console.log(err);
 //   }
 //   console.log(result);
+//   console.log("The ${params.name} department was added to the database.")
 // });
 
 //delete a department
@@ -28,6 +29,7 @@ console.log(rows);
 //       console.log(err);
 //     }
 //     console.log(result);
+//     console.log("The ${params.name} department was deleted from the database.");
 //   });
 
 //all info from roles table
@@ -39,3 +41,22 @@ db.query(sql, (err, rows) => {
 });
 
 //add a role
+// const params = ['Paralegal', 60000, 4];
+
+// db.query(`INSERT INTO roles (title, salary, department_id)
+//               VALUES (?,?,?)`, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+//   console.log(`The ${params.title} role was added to the database.`);
+// });
+
+//delete a role
+// db.query(`DELETE FROM roles WHERE id = ?`, 9, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//     // console.log(`The ${params.title} role was deleted from the database.`);
+//   });
