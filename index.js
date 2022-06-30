609c64ce75984f3b630825066b20dcf33dfbf181
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-30 16:29:07 -0400

    set up the rest of the methods within the Run class

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Department = require("./lib/Department");
 const Roles = require("./lib/Roles");
 const Employees = require("./lib/Employees");
 
-//choices Array for running the program
+//Inquirer Arrays
 choicesArr = ["View all departments", "Add a department", "Delete a department", "View all roles", "Add a role", "Delete a role", "Update a role's salary", "View all employees", "View an employee by their manager", "Update an employee's manager", "Add an employee", "Delete an Employee", "View the budget of each department", "Quit"
 ];
 
@@ -18,12 +18,26 @@ addRoleInq = [{
       name: "salary",
       message: "What is the salary of the new role?",
   },
+  //TODO:how to update departments when a new one is added?
   {
-      type: "text",
+      type: "list",
       name: "department",
       message: "What is the department of the new role?",
+      choices: ["Sales", "Engineering", "Finance", "Legal"]
   }];
 
+  updateRoleInq= [
+    {
+        type: "list",
+        name: "title",
+        message: "What is the title of the role?",
+        choices: ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"]
+  },
+{
+    type: "text",
+    name: "salary",
+    message: "What is the new salary of the role?",
+}]
 class Run {
  constructor () {
     this.roles = new Roles();
@@ -48,6 +62,7 @@ class Run {
       switch (action){
         case "View all departments":
         this.department.getDepartments();
+        // TODO: this doesn't display correctly and go back to initial prompt
         this.startProgram();
         break;
         case "Add a department":
@@ -135,17 +150,52 @@ addRole(){
     .then(({ title, salary, department }) => {
       this.roles.addRole(title, salary, department);
       console.log(`The ${title} role was added to the database.`);
-    //   //go back to the initial prompt
+      //go back to the initial prompt
       this.startProgram();
     });
 }
 
 deleteRole(){
-
+    inquirer
+    .prompt({
+      type: "text",
+      name: "title",
+      message: "Which role would you like to delete?",
+    })
+    //destructure name from the prompt object
+    .then(({ title }) => {
+      this.roles.deleteRole(title);
+      console.log(`The ${title} role was deleted from the database.`);
+      //go back to the initial prompt
+      this.startProgram();
+ });
 }
 
-updateRole(){}
+updateRole(){
+    inquirer
+    .prompt(updateRoleInq)
+    //destructure name from the prompt object
+    .then(({ title, salary }) => {
+      this.roles.updateRole(title, salary);
+      console.log(`The salary for the ${title} role has been updated in the database.`);
+      //go back to the initial prompt
+      this.startProgram();
+    })
+}
+
+addEmployee(){
 
 };
+
+deleteEmployee(){
+
+};
+
+endProgram(){
+    console.log("Goodbye!");
+    process.exit();
+}
+};
+
 
 new Run().welcomeMessage();
